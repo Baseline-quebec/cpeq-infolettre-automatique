5d675d7260497ad44fb7167d71c18c9c9fff3da7
Émile Turcotte <dev@emileturcotte.com>
2024-06-11 09:30:27 -0400

    Finalized provider initialization

--- iac/providers.tf
+++ iac/providers.tf
@@ -5,7 +5,17 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+
   required_version = ">= 1.8.0"
+
+  backend "azurerm" {
+    resource_group_name  = "Baseline-Core-Resources"
+    storage_account_name = "baseline0terraform"
+    container_name       = "cpeq"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+    use_azuread_auth     = true
+  }
 }
 
 provider "azurerm" {
