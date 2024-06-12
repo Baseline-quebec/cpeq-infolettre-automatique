00ac047c9d6f890357cbe8f7f49124cb8c0ea6e7
Émile Turcotte <dev@emileturcotte.com>
2024-06-12 19:01:18 -0400

    Adding AzureAD provider

--- iac/providers.tf
+++ iac/providers.tf
@@ -4,23 +4,22 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
   }
 
   required_version = ">= 1.8.0"
-
-  backend "azurerm" {
-    resource_group_name  = "Baseline-Core-Resources"
-    storage_account_name = "baseline0terraform"
-    container_name       = "cpeq"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-    use_azuread_auth     = true
-  }
 }
 
 provider "azurerm" {
   features {}
 
   # This provider is configured through environment variables.
-  # See variables/dev.tfvars for values
+}
+
+provider "azuread" {
+  # This provider is configured through environment variables.
 }
