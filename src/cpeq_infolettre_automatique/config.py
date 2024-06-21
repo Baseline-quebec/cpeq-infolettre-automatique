d50e1392305e3a37c508b04a1e28148aef4afbf6
Olivier Belhumeur <belhumeurolivier@gmail.com>
2024-06-21 15:01:06 +0000

    refactor(config.py): changed list[dict[str, str]] to set[str]

--- src/cpeq_infolettre_automatique/config.py
+++ src/cpeq_infolettre_automatique/config.py
@@ -4,6 +4,4 @@ EMBEDDING_MODEL = "text-embedding-3-large"
 TOKEN_ENCODING = "cl100k_base"  # noqa: S105
 MAX_TOKENS = 8000
 
-sitemaps = [
-    {"sitemap_id": "some-id"},
-]
+sitemap_ids: set[str] = {"some-id"}
