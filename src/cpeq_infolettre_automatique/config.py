d41823d299ca878118e20733fbc29d03cd1bd679
jsleb333 <jean-samuel.leboeuf@baseline.quebec>
2024-06-21 15:44:14 -0400

    feat(dependencies.py): add fastapi dependency injection for the app endpoints

--- src/cpeq_infolettre_automatique/config.py
+++ src/cpeq_infolettre_automatique/config.py
@@ -1,5 +1,15 @@
 """App configuration."""
 
-EMBEDDING_MODEL = "text-embedding-3-large"
-TOKEN_ENCODING = "cl100k_base"  # noqa: S105
-MAX_TOKENS = 8000
+from pathlib import Path
+from typing import Literal
+
+
+EMBEDDING_MODEL: Literal[
+    "ext-embedding-ada-002",
+    "text-embedding-3-small",
+    "text-embedding-3-large",
+] = "text-embedding-3-large"
+TOKEN_ENCODING: Literal["cl100k_base", "p50k_base", "r50k_base", "gpt2"] = "cl100k_base"
+MAX_TOKENS: int = 8000
+
+VECTORSTORE_CONTENT_FILEPATH: Path = Path("rubrics", "rubrics.json")
