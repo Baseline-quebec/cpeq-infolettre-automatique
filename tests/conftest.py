2a14a59739ea30db36a23b105692e2daeba914f6
jsleb333 <jean-samuel.leboeuf@baseline.quebec>
2024-06-25 12:40:16 -0400

    test(conftest,service): finish list of component fixtures, add happy path unit test for generate_newsletter

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Configuration for pytest. Add global fixtures here."""
 
-from datetime import datetime
+from datetime import date
+from typing import Any
 from unittest.mock import AsyncMock
 
 import pytest
@@ -11,24 +12,52 @@ from cpeq_infolettre_automatique.webscraper_io_client import WebScraperIoClient
 
 
 @pytest.fixture()
-def webscraper_io_client_fixture() -> WebScraperIoClient:
-    """Fixture for mocked WebScraperIoClient."""
-    return AsyncMock(spec=WebScraperIoClient)
-
-
-@pytest.fixture()
-def vectorstore_fixture() -> VectorStore:
-    """Fixture for mocked VectorStore."""
-    return AsyncMock(spec=VectorStore)
-
-
-@pytest.fixture()
 def news_fixture() -> News:
     """Fixture for a News object."""
     return News(
         title="Some title",
         content="Some content",
-        date=datetime(2024, 1, 1),  # noqa: DTZ001
-        rubric="Some rubric",
-        summary="Some summary",
+        date=date(2024, 1, 2),
+        rubric=None,
+        summary=None,
     )
+
+
+@pytest.fixture()
+def webscraper_io_client_fixture(news_fixture: News) -> WebScraperIoClient:
+    """Fixture for mocked WebScraperIoClient."""
+    webscraper_io_client_fixture = AsyncMock(spec=WebScraperIoClient)
+    webscraper_io_client_fixture.get_scraping_jobs = AsyncMock(
+        return_value=["job_id_1", "job_id_2"]
+    )
+    webscraper_io_client_fixture.get_scraping_job_data = AsyncMock(return_value=[news_fixture] * 2)
+    return webscraper_io_client_fixture
+
+
+@pytest.fixture()
+def vectorstore_fixture(news_fixture: News) -> VectorStore:
+    """Fixture for mocked VectorStore."""
+    vectorstore_fixture = AsyncMock(spec=VectorStore)
+    vectorstore_fixture.classify_rubric = AsyncMock(return_value="Some rubric")
+    vectorstore_fixture.get_examples = AsyncMock(return_value=[news_fixture] * 3)
+    return vectorstore_fixture
+
+
+@pytest.fixture()
+def news_repository_fixture() -> Any:
+    """Fixture for mocked NewsRepository."""
+    return AsyncMock()
+
+
+@pytest.fixture()
+def summary_generator_fixture() -> Any:
+    """Fixture for mocked NewsRepository."""
+    summary_generator_fixture = AsyncMock()
+    summary_generator_fixture.generate_summary = AsyncMock(return_value="Some summary")
+    return summary_generator_fixture
+
+
+@pytest.fixture()
+def newsletter_formatter_fixture() -> Any:
+    """Fixture for mocked NewsRepository."""
+    return AsyncMock()
--- tests/service_test.py
+++ tests/service_test.py
@@ -1,6 +1,7 @@
 """Tests for service class."""
 
-from unittest.mock import AsyncMock
+from datetime import date
+from typing import Any
 
 import pytest
 
@@ -11,16 +12,21 @@ from cpeq_infolettre_automatique.webscraper_io_client import WebScraperIoClient
 
 @pytest.fixture()
 def service_fixture(
-    webscraper_io_client_fixture: WebScraperIoClient, vectorstore_fixture: VectorStore
+    webscraper_io_client_fixture: WebScraperIoClient,
+    vectorstore_fixture: VectorStore,
+    news_repository_fixture: Any,
+    summary_generator_fixture: Any,
+    newsletter_formatter_fixture: Any,
 ) -> Service:
     """Fixture for mocked service."""
     service = Service(
         webscraper_io_client=webscraper_io_client_fixture,
-        news_repository=AsyncMock(),
+        news_repository=news_repository_fixture,
         vectorstore=vectorstore_fixture,
-        summary_generator=AsyncMock(),
-        newsletter_formatter=AsyncMock(),
+        summary_generator=summary_generator_fixture,
+        newsletter_formatter=newsletter_formatter_fixture,
     )
+    service._prepare_dates = lambda *_: (date(2024, 1, 1), date(2024, 1, 7))
     return service
 
 
@@ -29,7 +35,18 @@ class TestService:
 
     @staticmethod
     @pytest.mark.asyncio()
-    async def test_generate_newsletter(
+    async def generate_newsletter__given_happy_path__returns_list_of_news(
         service_fixture: Service,
     ) -> None:
-        """Test generating a newsletter."""
+        """Test that the newsletter generation flow and logic operates as intented given expected situation."""
+        service_fixture._format_newsletter = lambda news: news  # type: ignore[return-value, assignment]
+        news_list = await service_fixture.generate_newsletter()
+        expected_number_of_news = 4
+        assert len(news_list) == expected_number_of_news
+        assert service_fixture.webscraper_io_client.get_scraping_jobs.called
+        assert service_fixture.webscraper_io_client.get_scraping_job_data.called
+        assert service_fixture.vectorstore.classify_rubric.called
+        assert service_fixture.vectorstore.get_examples.called
+        assert service_fixture.summary_generator.generate_summary.called
+        assert service_fixture.webscraper_io_client.delete_scraping_jobs.called
+        assert service_fixture.repository.save_news.called
