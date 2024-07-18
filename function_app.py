189e0544d8d0223b89dc867d5e7de011aa863f60
Émile Turcotte <dev@emileturcotte.com>
2024-07-18 17:38:23 -0400

    Add comment

--- function_app.py
+++ function_app.py
@@ -5,4 +5,5 @@ import azure.functions as func
 from cpeq_infolettre_automatique.api import app as fastapi_app
 
 
+# https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference-python?tabs=asgi%2Capplication-level&pivots=python-mode-decorators#web-frameworks
 app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
