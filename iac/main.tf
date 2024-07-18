be5ed4df578f76019a1285ddb558ccab5c7b102e
Émile Turcotte <dev@emileturcotte.com>
2024-07-18 08:43:43 -0400

    maybe

--- iac/main.tf
+++ iac/main.tf
@@ -39,12 +39,18 @@ resource "azurerm_linux_function_app" "function_app" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  service_plan_id            = azurerm_service_plan.service_plan.id
-  https_only                 = true
+  storage_account_name        = azurerm_storage_account.storage.name
+  storage_account_access_key  = azurerm_storage_account.storage.primary_access_key
+  service_plan_id             = azurerm_service_plan.service_plan.id
+  https_only                  = true
+  functions_extension_version = "~4"
 
-  site_config {}
+  site_config {
+    application_stack {
+      python_version     = "3.11"
+      use_custom_runtime = false
+    }
+  }
 
   tags = {
     environment = var.environment
