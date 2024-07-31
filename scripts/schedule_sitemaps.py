e74a06fa0d686e57aedc8c5544e166a5d30cab5e
Olivier Belhumeur <belhumeurolivier@gmail.com>
2024-07-31 16:42:25 +0000

    fix(webscraper_scheduler.py): typo removal

--- scripts/schedule_sitemaps.py
+++ scripts/schedule_sitemaps.py
@@ -1,11 +1,10 @@
-"""File implementing the WebscrapperIO Cron sitemaps."""
+"""File implementing the WebscraperIO Cron sitemaps."""
 
 import enum
 import logging
 
 import httpx
 from decouple import config
-from pydantic import ValidationError
 
 from cpeq_infolettre_automatique.webscraper_io_client import WebscraperIoClient
 
@@ -26,11 +25,11 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-class WebScrapperIOCronScheduler(WebscraperIoClient):
+class WebScraperIOCronScheduler(WebscraperIoClient):
     """A class to schedule sitemaps using the WebScraper.io API."""
 
     def __init__(self, http_client: httpx.AsyncClient, api_token: str) -> None:
-        """Initialize the WebScrapperCronScheduler with the provided API token.
+        """Initialize the WebScraperCronScheduler with the provided API token.
 
         Args:
             http_client (httpx.AsyncClient): The AsyncClient used for API calls.
@@ -43,14 +42,9 @@ class WebScrapperIOCronScheduler(WebscraperIoClient):
 
         Note:
             https://webscraper.io/documentation/web-scraper-cloud/api#:~:text=Enable%20Sitemap%20Scheduler
-
         Args:
             sitemap_id: ID of the sitemap to schedule.
             weekday: The weekday to schedule the sitemap on.
-
-        Raises:
-            httpx.HTTPStatusError: If the HTTP status code is not 2xx.
-            httpx.RequestError: If an error occurs while issuing the request
         """
         url: str = f"{self._base_url}/sitemap/{sitemap_id}/enable-scheduler"
         data: dict[str, str | int] = {
@@ -71,43 +65,26 @@ class WebScrapperIOCronScheduler(WebscraperIoClient):
             headers=self._headers,
             params={"api_token": self._api_token},
         )
-
-        try:
-            response.raise_for_status()
-        except httpx.HTTPStatusError as e:
-            logger.exception(
-                "HTTP Code %s while scheduling sitemap ID %s.",
-                e.response.status_code,
-                sitemap_id,
-            )
-        except httpx.RequestError:
-            logger.exception("Error issuing POST request at URL %s.", url)
+        response.raise_for_status()
 
     async def schedule_sitemaps(self, weekday: Weekday) -> None:
         """Schedules all sitemaps for scraping on a specific weekday.
 
         Args:
             weekday: The weekday to schedule the sitemaps on.
-
-        Raises:
-            httpx.HTTPStatusError: If the HTTP status code is not 2xx.
-            httpx.RequestError: If an error occurs while issuing the request
         """
         sitemaps = await self.get_sitemaps()
         for sitemap in sitemaps:
-            try:
-                await self.schedule_sitemap(sitemap["id"], weekday)
-            except ValidationError:
-                logger.exception("Validation error occurred.")
+            await self.schedule_sitemap(sitemap["id"], weekday)
 
 
 async def main(weekday: Weekday) -> None:
     """Schedule all sitemaps for scraping on a specific weekday."""
     async with httpx.AsyncClient() as client:
-        webscrapper_io_scheduler = WebScrapperIOCronScheduler(
+        webscraper_io_scheduler = WebScraperIOCronScheduler(
             client, api_token=config("WEBSCRAPER_IO_API_KEY")
         )
-        await webscrapper_io_scheduler.schedule_sitemaps(weekday)
+        await webscraper_io_scheduler.schedule_sitemaps(weekday)
 
 
 if __name__ == "__main__":
