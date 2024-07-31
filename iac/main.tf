4069dbaecb8e3b88ae4adf1d08fab2f442fd477b
Émile Turcotte <dev@emileturcotte.com>
2024-07-31 12:42:44 -0400

    Revert to managed identity

--- iac/main.tf
+++ iac/main.tf
@@ -21,31 +21,12 @@ resource "azurerm_container_registry" "acr" {
   }
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_role_assignment" "build_pipeline" {
   scope                = azurerm_container_registry.acr.id
   principal_id         = var.build_pipeline_object_id
   role_definition_name = "AcrPush"
-}
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_key_vault" "keyvault" {
-  name                        = "kvcpeq${var.environment}${var.location}"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  enable_rbac_authorization   = true
-
-  sku_name = "standard"
-}
-
-resource "azurerm_role_assignment" "terraform_secrets_admin" {
-  scope                = azurerm_key_vault.keyvault.id
-  principal_id         = data.azurerm_client_config.current.object_id
-  role_definition_name = "Key Vault Secrets Officer"
 }
 
 resource "azurerm_user_assigned_identity" "identity" {
@@ -59,28 +40,19 @@ resource "azurerm_user_assigned_identity" "identity" {
   }
 }
 
-resource "azurerm_role_assignment" "keyvault_secret_read" {
-  scope                = azurerm_key_vault.keyvault.id
+resource "azurerm_role_assignment" "acrpull" {
+  scope                = azurerm_container_registry.acr.id
   principal_id         = azurerm_user_assigned_identity.identity.principal_id
-  role_definition_name = "Key Vault Secrets User"
+  role_definition_name = "AcrPull"
 }
 
 resource "time_sleep" "wait_rbac_propagation" {
-  depends_on      = [azurerm_role_assignment.keyvault_secret_read]
+  depends_on      = [azurerm_role_assignment.acrpull]
   create_duration = "1m"
 
   triggers = {
-    container_app = azurerm_role_assignment.keyvault_secret_read.scope
-    terraform     = azurerm_role_assignment.terraform_secrets_admin.scope
+    container_app = azurerm_role_assignment.acrpull.scope
   }
-}
-
-resource "azurerm_key_vault_secret" "acr_password" {
-  depends_on = [time_sleep.wait_rbac_propagation]
-
-  name         = "ContainerRegistry--AdminPassword"
-  value        = azurerm_container_registry.acr.admin_password
-  key_vault_id = azurerm_key_vault.keyvault.id
 }
 
 resource "azurerm_container_app_environment" "environment" {
@@ -118,16 +90,9 @@ resource "azurerm_container_app" "app" {
     identity_ids = [azurerm_user_assigned_identity.identity.id]
   }
 
-  secret {
-    name                = "acr-admin-password"
-    identity            = azurerm_user_assigned_identity.identity.id
-    key_vault_secret_id = azurerm_key_vault_secret.acr_password.id
-  }
-
   registry {
-    server               = azurerm_container_registry.acr.login_server
-    username             = azurerm_container_registry.acr.admin_username
-    password_secret_name = "acr-admin-password"
+    server   = azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.identity.id
   }
 
   tags = {
